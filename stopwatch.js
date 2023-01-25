092aad8a81c37f87d24b346a0a3a32558603ad9a
kondamadhavi <kondamadhavi2728@gmail.com>
2023-01-25 19:28:57 +0530

    first commit

---
+++ stopwatch.js
@@ -0,0 +1,11 @@
+window.onload=function()
+{
+    var seconds=0;
+    var tens=0;
+    var appendTens=document.getElementById("tens")
+    var appendSeconds=document.getElementById("seconds")
+    var buttonStart=document.getElementById("button-start")
+    var buttonStop=document.getElementById("button-stop")
+    var buttonReset=document.getElementById("button-reset")
+    var interval
+}
